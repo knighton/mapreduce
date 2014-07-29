ebc3805554932a76e50feef427f192a162b7f072
James Knighton <iamknighton@gmail.com>
2014-07-29 12:39:09 -0700

    move map output sorting and counting into mappers

--- mapreduce/map.py
+++ mapreduce/map.py
@@ -3,6 +3,7 @@
 from argparse import ArgumentParser
 import imp
 import json
+import os
 
 ap = ArgumentParser()
 ap.add_argument('--shard', type=int,
@@ -22,18 +23,30 @@ assert args.work_dir
 
 module = imp.load_source('module', args.mr)
 
+# get our slice of the input files.
 ff = args.input_files
 assert args.n_shards <= len(ff)
 a = len(ff) * args.shard / args.n_shards
 z = len(ff) * (args.shard + 1) / args.n_shards
 ff = ff[a:z]
 
-out_f = open('%s/map.out.%d' % (args.work_dir, args.shard), 'w')
+# process each file.
+out_fn = '%s/map.out.%d' % (args.work_dir, args.shard)
+out_f = open(out_fn, 'w')
+count = 0
 for f in ff:
     for line in open(f):
         for key, value in module.map(line):
             j = {'kv': [key, value]}
             out_f.write(json.dumps(j) + '\n')
+            count += 1
 out_f.close()
 
+f = '%s/map.out_count.%d' % (args.work_dir, args.shard)
+open(f, 'w').write(str(count))
+
+# sort the results for merge step.
+os.system('sort %s -o %s' % (out_fn, out_fn))
+
+# finally note that we are done.
 open('%s/map.done.%d' % (args.work_dir, args.shard), 'w').write('')
--- mapreduce/shuffle.py
+++ mapreduce/shuffle.py
@@ -3,7 +3,6 @@
 from argparse import ArgumentParser
 import glob
 import heapq
-import os
 
 ap = ArgumentParser()
 ap.add_argument('--work_dir', type=str,
@@ -15,19 +14,19 @@ args = ap.parse_args()
 assert args.work_dir
 assert args.n_reduce_shards
 
+# count exactly how many input lines we have so we can balance work.
 num_entries = 0
-in_ff = sorted(glob.glob('%s/map.out.*' % args.work_dir))
-sources = []
-for f in in_ff:
-    for line in open(f):
-        num_entries += 1
-    print 'Sorting %s' % f
-    os.system('sort %s -o %s' % (f, f))
-    sources.append(open(f))
+count_ff = glob.glob('%s/map.out_count.*' % args.work_dir)
+for f in count_ff:
+    n = int(open(f).read())
+    num_entries += n
 
-print 'Merging'
-out_ff = map(lambda i: open('mapreduce/tmp/reduce.in.%d' % i, 'w'),
+in_ff = sorted(glob.glob('%s/map.out.*' % args.work_dir))
+sources = map(open, in_ff)
+
+out_ff = map(lambda i: open('%s/reduce.in.%d' % (args.work_dir, i), 'w'),
              range(args.n_reduce_shards))
+
 count = 0
 for line in heapq.merge(*sources):
     index = count * len(out_ff) / num_entries
