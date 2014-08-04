68d2d5cb23f5d8ac7f45e3a0ca65656a3dd79fd1
James Knighton <iamknighton@gmail.com>
2014-08-04 16:17:43 -0700

    balance mapper inputs by assigning slices of files to mappers, not whole files

--- mapreduce/map.py
+++ mapreduce/map.py
@@ -3,6 +3,7 @@
 from argparse import ArgumentParser
 import imp
 import json
+import math
 import os
 
 ap = ArgumentParser()
@@ -23,33 +24,61 @@ args = ap.parse_args()
 assert 0 <= args.shard < args.n_shards
 assert args.work_dir
 
+
+def each_input_line(input_files, shard, n_shards):
+    # assign slices of each file to shards.
+    slice_assignments = []
+    for i in range(n_shards):
+        slice_assignments += [i] * len(input_files)
+
+    # get which files this shard is using (partially or the whole file).
+    a = len(input_files) * shard / n_shards
+    z = len(input_files) * (shard + 1) / float(n_shards)
+    z = int(math.ceil(z))
+
+    # for each input file, yield the slices we want from it.
+    for i in range(a, z):
+        aa = n_shards * i
+        zz = n_shards * (i + 1)
+        assign = slice_assignments[aa:zz]
+        f = input_files[i]
+        f = open(f)
+        done = False
+        while not done:
+            for j in range(n_shards):
+                s = f.readline()
+                if not s:
+                    done = True
+                    break
+                if assign[j] == shard:
+                    yield s
+
+
+# find the map function.
 map_module = imp.load_source('map_module', args.map_module)
 map_func = getattr(map_module, args.map_func)
 
-# get our slice of the input files.
-ff = args.input_files
-assert args.n_shards <= len(ff)
-a = len(ff) * args.shard / args.n_shards
-z = len(ff) * (args.shard + 1) / args.n_shards
-ff = ff[a:z]
 
-# process each file.
+# process each line of input.
 out_fn = '%s/map.out.%d' % (args.work_dir, args.shard)
 out_f = open(out_fn, 'w')
 count = 0
-for f in ff:
-    for line in open(f):
-        for key, value in map_func(line):
-            j = {'kv': [key, value]}
-            out_f.write(json.dumps(j) + '\n')
-            count += 1
+for line in each_input_line(args.input_files, args.shard, args.n_shards):
+    for key, value in map_func(line):
+        j = {'kv': [key, value]}
+        out_f.write(json.dumps(j) + '\n')
+        count += 1
 out_f.close()
 
+
+# write how many entries were written for reducer balancing purposes.
 f = '%s/map.out_count.%d' % (args.work_dir, args.shard)
 open(f, 'w').write(str(count))
 
+
 # sort the results for merge step.
 os.system('sort %s -o %s' % (out_fn, out_fn))
+
 
 # finally note that we are done.
 open('%s/map.done.%d' % (args.work_dir, args.shard), 'w').write('')
