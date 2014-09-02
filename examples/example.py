5cafbd8b2036385441dd843d14f173681a7c6969
Eugene Scherba <escherba@livefyre.com>
2014-09-01 17:01:14 -0700

    update example script / step interface

--- examples/example.py
+++ examples/example.py
@@ -1,14 +1,18 @@
 import re
 import glob
 import json
-from mrdomino import mapreduce
+from mrdomino import mapreduce, MRStep, MRSettings
+
+
+def get_tld(domain):
+    return re.match(r'^.*\b([^\.]+\.[^\.]+)$', domain).group(1)
 
 
 def map1(_, line, increment_counter):
     j = json.loads(line)
     key = j[u'object'][u'user_id']
     uname, domain = key.split("@")
-    tld = re.match(r'^.*\b([^\.]+\.[^\.]+)$', domain).group(1)
+    tld = get_tld(domain)
     increment_counter("TLD map1", tld, 1)
     yield key, 1
 
@@ -20,7 +24,7 @@ def combine1(key, vals):
 def reduce1(key, vals, increment_counter):
     total = sum(vals)
     uname, domain = key.split("@")
-    tld = re.match(r'^.*\b([^\.]+\.[^\.]+)$', domain).group(1)
+    tld = get_tld(domain)
     increment_counter("TLD reduce1", tld, total)
     yield key, total    # username -> count of posts
 
@@ -31,45 +35,43 @@ def combine2(key, vals):
 
 def map2(key, val, increment_counter):
     uname, domain = key.split("@")
-    tld = re.match(r'^.*\b([^\.]+\.[^\.]+)$', domain).group(1)
+    tld = get_tld(domain)
     increment_counter("TLD map2", tld, val)
     yield domain, val
 
 
 def reduce2(key, vals, increment_counter):
     total = sum(vals)
-    tld = re.match(r'^.*\b([^\.]+\.[^\.]+)$', key).group(1)
+    tld = get_tld(key)
     increment_counter("TLD reduce2", tld, total)
     yield key, total
 
 
 def main():
     steps = [
-        {
-            'mapper': map1,
-            'n_mappers': 2,
-            'combiner': combine1,
-            'reducer': reduce1,
-            'n_reducers': 3,
-        },
-        {
-            'mapper': map2,
-            'n_mappers': 5,
-            'combiner': combine2,
-            'reducer': reduce2,
-            'n_reducers': 4,
-        }
+        MRStep(
+            mapper=map1,
+            combiner=combine1,
+            reducer=reduce1,
+            n_mappers=2,
+            n_reducers=3
+        ),
+        MRStep(
+            mapper=map2,
+            combiner=combine2,
+            reducer=reduce2,
+            n_mappers=5,
+            n_reducers=4
+        )
     ]
-
-    settings = {
-        'use_domino': False,
-        'n_concurrent_machines': 2,
-        'n_shards_per_machine': 3,
-        'input_files': glob.glob('./data/2014-01-18.detail.10000'),
-        'output_dir': 'out',
-        'tmp_dir': 'tmp'
-    }
-
+    settings = MRSettings(
+        input_files=glob.glob('./data/2014-01-18.detail.10000'),
+        output_dir='out',
+        tmp_dir='tmp',
+        use_domino=False,
+        n_concurrent_machines=2,
+        n_shards_per_machine=3
+    )
     mapreduce(steps, settings)
 
 
--- mrdomino/__init__.py
+++ mrdomino/__init__.py
@@ -16,26 +16,85 @@ logger.addHandler(handler)
 EXEC_SCRIPT = resource_filename(__name__, "exec.sh")
 
 
+class MRStep(object):
+    def __init__(self, mapper, reducer, combiner=None, n_mappers=2,
+                 n_reducers=2):
+
+        # do some basic type checking to verify that we pass callables.
+        assert hasattr(mapper, '__call__')
+        self.mapper = mapper
+        assert hasattr(reducer, '__call__')
+        self.reducer = reducer
+        assert combiner is None or hasattr(combiner, '__call__')
+        self.combiner = combiner
+        assert isinstance(n_mappers, int)
+        self.n_mappers = n_mappers
+        assert isinstance(n_reducers, int)
+        self.n_reducers = n_reducers
+
+    @property
+    def mapper_func(self):
+        return self.mapper.func_name
+
+    @property
+    def mapper_module(self):
+        return self.mapper.func_globals['__file__']
+
+    @property
+    def combiner_func(self):
+        return self.combiner.func_name
+
+    @property
+    def combiner_module(self):
+        return self.combiner.func_globals['__file__']
+
+    @property
+    def reducer_func(self):
+        return self.reducer.func_name
+
+    @property
+    def reducer_module(self):
+        return self.reducer.func_globals['__file__']
+
+
+class MRSettings(object):
+    def __init__(self, input_files, output_dir, tmp_dir, use_domino=False,
+                 n_concurrent_machines=2, n_shards_per_machine=4):
+
+        assert isinstance(input_files, list)
+        self.input_files = input_files
+        assert isinstance(output_dir, str)
+        self.output_dir = output_dir
+        assert isinstance(tmp_dir, str)
+        self.tmp_dir = tmp_dir
+        assert isinstance(use_domino, bool)
+        self.use_domino = use_domino
+        assert isinstance(n_concurrent_machines, int)
+        self.n_concurrent_machines = n_concurrent_machines
+        assert isinstance(n_shards_per_machine, int)
+        self.n_shards_per_machine = n_shards_per_machine
+
+
 def mapreduce(steps, settings):
 
     step_count = len(steps)
 
     # if temporary directory root does not exist, create one
-    tmp_root = settings['tmp_dir']
+    tmp_root = settings.tmp_dir
     if not os.path.exists(tmp_root):
         os.makedirs(tmp_root)
     tmp_dirs = [mkdtemp(dir=tmp_root, prefix="step%d." % i)
                 for i in range(step_count)]
 
-    input_file_lists = [settings['input_files']]
+    input_file_lists = [settings.input_files]
     for step, out_dir in zip(steps, tmp_dirs):
-        n_reducers = step['n_reducers']
+        n_reducers = step.n_reducers
         reduce_format = os.path.join(out_dir, 'reduce.out.%d')
         ff = [reduce_format % n for n in range(n_reducers)]
         input_file_lists.append(ff)
 
     # if output directory root does not exist, create one
-    output_dir = settings['output_dir']
+    output_dir = settings.output_dir
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -47,23 +106,22 @@ def mapreduce(steps, settings):
             '--input_files', ' '.join(input_file_lists[i]),
             '--work_dir', tmp_dirs[i],
             '--output_dir', output_dir,
-            '--map_module', step['mapper'].func_globals['__file__'],
-            '--map_func', step['mapper'].func_name,
-            '--n_map_shards', step['n_mappers'],
-            '--reduce_module', step['reducer'].func_globals['__file__'],
-            '--reduce_func', step['reducer'].func_name,
-            '--n_reduce_shards', step['n_reducers'],
-            '--use_domino', int(settings['use_domino']),
-            '--n_concurrent_machines', settings['n_concurrent_machines'],
-            '--n_shards_per_machine', settings['n_shards_per_machine']
+            '--map_module', step.mapper_module,
+            '--map_func', step.mapper_func,
+            '--n_map_shards', step.n_mappers,
+            '--reduce_module', step.reducer_module,
+            '--reduce_func', step.reducer_func,
+            '--n_reduce_shards', step.n_reducers,
+            '--use_domino', int(settings.use_domino),
+            '--n_concurrent_machines', settings.n_concurrent_machines,
+            '--n_shards_per_machine', settings.n_shards_per_machine
         ]
 
         # combiner is optional
-        combiner = step.get('combiner')
-        if combiner is not None:
+        if step.combiner is not None:
             cmd_opts.extend([
-                '--combine_func', combiner.func_name,
-                '--combine_module', combiner.func_globals['__file__']])
+                '--combine_func', step.combiner_func,
+                '--combine_module', step.combiner_module])
 
         cmd = util.create_cmd(cmd_opts)
         util.wait_cmd(cmd, logger, "Step %d" % i)
